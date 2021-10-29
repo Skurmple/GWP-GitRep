d65b4fccc4c37c7e971143bbbf3540ac73267ab7
Mr-Keegzy <lewismckeegan2002@gmail.com>
2021-10-29 19:29:29 +0100

    Main menu import
    I have imported the main menu stuff, functionality between menu's work as well >:D

---
+++ Ocean Cleanup Net Prototype/Assets/C#/Menu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    int sceneBuildIndex;
+
+    public GameObject mainMenu;
+    public GameObject helpMenu;
+
+    void Start()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Help()
+    {
+        mainMenu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        helpMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
