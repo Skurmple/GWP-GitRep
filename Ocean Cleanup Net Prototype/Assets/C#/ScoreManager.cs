bda7e7dd507ebf0016a0536fd235c8d8e8d5ae92
LachlanStewart-1 <61358394+LachlanStewart-1@users.noreply.github.com>
2021-11-01 09:37:20 +0000

    UI
    Added in the top left UI elements

--- Ocean Cleanup Net Prototype/Assets/C#/ScoreManager.cs
+++ Ocean Cleanup Net Prototype/Assets/C#/ScoreManager.cs
@@ -7,19 +7,51 @@ public class ScoreManager : MonoBehaviour
 {
 
     public TrashNet player;
-    public Text scoreUI;
-    public Text trashUI;
+    //public Text scoreUI;
     public Text plasticTrashUI;
     public Text metalTrashUI;
     public Text glassTrashUI;
 
+    public TrashNet trashNet;
+    public GameObject netCounter0, netCounter1, netCounter2, netCounter3, netCounter4, netCounter5;
 
     public void Update()
     {
-        scoreUI.text = "Score: " + player.score.ToString();
-        trashUI.text = "Trash in Net: " + player.trashList.Count.ToString() + "/5";
-        plasticTrashUI.text = "Plastic: " + player.plasticTrashAmt.ToString();
-        metalTrashUI.text = "Metal: " + player.metalTrashAmt.ToString();
-        glassTrashUI.text = "Glass: " + player.glassTrashAmt.ToString();
+        //scoreUI.text = player.score.ToString();
+        plasticTrashUI.text = player.plasticTrashAmt.ToString();
+        metalTrashUI.text = player.metalTrashAmt.ToString();
+        glassTrashUI.text = player.glassTrashAmt.ToString();
+
+        switch (trashNet.trashList.Count)
+        {
+            case 0:
+                netCounter0.SetActive(true);
+                netCounter1.SetActive(false);
+                netCounter2.SetActive(false);
+                netCounter3.SetActive(false);
+                netCounter4.SetActive(false);
+                netCounter5.SetActive(false);
+                break;
+            case 1:
+                netCounter0.SetActive(false);
+                netCounter1.SetActive(true);
+                break;
+            case 2:
+                netCounter1.SetActive(false);
+                netCounter2.SetActive(true);
+                break;
+            case 3:
+                netCounter2.SetActive(false);
+                netCounter3.SetActive(true);
+                break;
+            case 4:
+                netCounter3.SetActive(false);
+                netCounter4.SetActive(true);
+                break;
+            case 5:
+                netCounter4.SetActive(false);
+                netCounter5.SetActive(true);
+                break;
+        }
     }
 }
