fa2b3c43ab11bbc4bc2ac64a3519eba557e29995
Swueld <vojtech.kuba.cz@gmail.com>
2021-12-11 17:58:58 +0000

    Parallax working

--- Ocean Cleanup Net Prototype/Assets/C#/Parallax.cs
+++ Ocean Cleanup Net Prototype/Assets/C#/Parallax.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private Vector2 dimesions, startpos;
+    private Vector2 dimesions, startPos;
+    private Vector3 camStartPos;
     public GameObject cam;
     public float parallaxEffect;
     
     void Start()
     {
-        startpos = transform.position;
-        dimesions = GetComponent<SpriteRenderer>().bounds.size;
+        startPos = transform.position;
+        camStartPos = cam.transform.position;
+        //dimesions = GetComponent<SpriteRenderer>().bounds.size;
         //startposX = transform.position.x;
         //startposY = transform.position.y;
         //length = GetComponent<SpriteRenderer>().bounds.size.x;
@@ -20,7 +22,8 @@ public class Parallax : MonoBehaviour
 
     void Update()
     {
-        Vector2 dist = cam.transform.position * parallaxEffect;
-        transform.position = new Vector3(startpos.x + dist.x, startpos.y + dist.y, transform.position.z);
+        Vector3 updatedCamPos = camStartPos - cam.transform.position;
+        Vector2 dist = updatedCamPos * parallaxEffect;
+        transform.position = new Vector3(startPos.x + dist.x, startPos.y + dist.y, transform.position.z);
     }
 }
