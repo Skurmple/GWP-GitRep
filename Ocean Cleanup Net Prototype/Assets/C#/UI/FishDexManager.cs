97630a3b8c5fb9c3665d61d67eb22e9fdf7fcbb2
Mr-Keegzy <lewismckeegan2002@gmail.com>
2022-03-29 19:52:18 +0100

    More Journal Progress

--- Ocean Cleanup Net Prototype/Assets/C#/UI/FishDexManager.cs
+++ Ocean Cleanup Net Prototype/Assets/C#/UI/FishDexManager.cs
@@ -2,166 +2,153 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FishDexManager : MonoBehaviour
 {
-    public Image Nautilus;
-    public Image SeaTurtle;
-    public Image Salmon;
-    public Image Pufferfish;
+    //Level 1
+    public Image AtlanticSalmon;
+    public Image Otter;
+    public Image JellyFish;
+    public Image Ling;
+    public Image RedFish;
+    public Image Saithe;
+
+    /*
+    public Image Polychaete;
+    public Image Catshark;
+    public Image Octopus;
+    public Image Starfish;
+    public Image Shrimp;                IGNORE FOR NOW     Still have to decide which creatures go where
+    public Image FeatherStar;
+    public Image SeaFan;
+    public Image Sponge;
+    public Image SeaAnemone;
+    */
+
+    //Level 2
+    public Image Seal;
+    public Image KillerWhale;
+    public Image BaskingShark;
+    public Image Dolphin;
+
+    //Level 3
+    public Image Oarfish;
+    public Image Alicella;
     public Image AnglerFish;
-    public Image SwordFish;
 
-    public Image SeaTurtle_NotScanned;
-    public Sprite SeaTurtle_Scanned;
-
-    public Image Salmon_NotScanned;
-    public Sprite Salmon_Scanned;
-
-    public Image Pufferfish_NotScanned;
-    public Sprite Pufferfish_Scanned;
-
-    public Image AnglerFish_NotScanned;
-    public Sprite AnglerFish_Scanned;
-
-    public Image SwordFish_NotScanned;
-    public Sprite Swordfish_Scanned;
-
-    public Text nautilus;
-    public Text oarfish;
-    public Text salmon;
-    public Text pufferfish;
-    public Text anglerfish;
-    public Text squid;
-    public Text seaTurtle;
-    public Text swordfish;
-
+    //checks to see if the fish has been selected or scaneed
     public bool[] hasScanned;
+    public bool[] fishSelected;
 
-    public GameObject NautilusInfo;
-    public GameObject SeaTurtleInfo;
+    //UI Screens GameObjects
+    public GameObject LevelSelective;
+    public GameObject Level1;
+    public GameObject Level2;
+    public GameObject Level3;
 
-    public GameObject Fishdex;
+    //text in front of sea creature images
+    public GameObject[] headings;
 
-    public bool[] opened;
-
-    public GameObject DrawingTurtle;
-    public GameObject PictureTurtle;
-
-    public GameObject DrawingNautilus;
-    public GameObject PictureNautilus;
-
-
-
-    void Update()
+    private void Update()
     {
         DiscoveryOfSpecies();
+        ScannedFish();
+    }
+
+    //opening & closing the menus
+    public void Level_1()
+    {
+        Level1.SetActive(true);
+        Level2.SetActive(false);
+        Level3.SetActive(false);
+        LevelSelective.SetActive(false);
+    }
+    public void Level_2()
+    {
+        Level1.SetActive(false);
+        Level2.SetActive(true);
+        Level3.SetActive(false);
+        LevelSelective.SetActive(false);
+    }
+    public void Level_3()
+    {
+        Level1.SetActive(false);
+        Level2.SetActive(false);
+        Level3.SetActive(true);
+        LevelSelective.SetActive(false);
+    }
+    public void Back()
+    {
+        Level1.SetActive(false);
+        Level2.SetActive(false);
+        Level3.SetActive(false);
+        LevelSelective.SetActive(true);
     }
 
     public void DiscoveryOfSpecies()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            AtlanticSalmon.color = new Color32(255, 255, 255, 255);
+            Otter.color = new Color32(255, 255, 255, 255);
+            JellyFish.color = new Color32(255, 255, 255, 255);
+            Ling.color = new Color32(255, 255, 255, 255);
+            RedFish.color = new Color32(255, 255, 255, 255);
+            Saithe.color = new Color32(255, 255, 255, 255);
+
+            /*
+            Polychaete.color = new Color32(255, 255, 255, 255);
+            Catshark.color = new Color32(255, 255, 255, 255);
+            Octopus.color = new Color32(255, 255, 255, 255);
+            Starfish.color = new Color32(255, 255, 255, 255);
+            Shrimp.color = new Color32(255, 255, 255, 255);                 AGAIN IGNORE
+            FeatherStar.color = new Color32(255, 255, 255, 255);
+            SeaFan.color = new Color32(255, 255, 255, 255);
+            Sponge.color = new Color32(255, 255, 255, 255);
+            SeaAnemone.color = new Color32(255, 255, 255, 255);
+            */
+
+            headings[0].GetComponent<Text>().text = "Salmon";
+            headings[1].GetComponent<Text>().text = "Otter";
+            headings[2].GetComponent<Text>().text = "Jellyfish";
+            headings[3].GetComponent<Text>().text = "Ling";
+            headings[4].GetComponent<Text>().text = "Redfish";
+            headings[5].GetComponent<Text>().text = "Saithe";
+
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            Seal.color = new Color32(255, 255, 255, 255);
+            KillerWhale.color = new Color32(255, 255, 255, 255);
+            BaskingShark.color = new Color32(255, 255, 255, 255);
+            Dolphin.color = new Color32(255, 255, 255, 255);
+
+            headings[6].GetComponent<Text>().text = "Seal";
+            headings[7].GetComponent<Text>().text = "Killer Whale";
+            headings[8].GetComponent<Text>().text = "Basking Shark";
+            headings[9].GetComponent<Text>().text = "Dolphin";
+        }
+    }
+    private void ScannedFish()
+    {
         if (hasScanned[0])
         {
-            Nautilus.color = new Color32(255, 255, 255, 255);
-            nautilus.text = "Nautilus";
+            Oarfish.color = new Color32(255, 255, 255, 255);
+            headings[10].GetComponent<Text>().text = "Oarfish";
         }
-        else
-        {
-            Nautilus.color = new Color32(0, 0, 0, 100);
-        }
-
         if (hasScanned[1])
         {
-            SeaTurtle_NotScanned.sprite = SeaTurtle_Scanned;
-            seaTurtle.text = "Sea Turtle";
+            Alicella.color = new Color32(255, 255, 255, 255);
+            headings[11].GetComponent<Text>().text = "Alicella";
         }
-        else
-        {
-            SeaTurtle.color = new Color32(0, 0, 0, 100);
-        }
-
         if (hasScanned[2])
         {
-            Salmon.color = new Color32(255, 255, 255, 255);
-            salmon.text = "Salmon";
-        }
-        else
-        {
-            Salmon.color = new Color32(0, 0, 0, 100);
-        }
-
-        if (hasScanned[3])
-        {
-            Pufferfish.color = new Color32(255, 255, 255, 255);
-            pufferfish.text = "Pufferfish";
-        }
-        else
-        {
-            Pufferfish.color = new Color32(0, 0, 0, 100);
-        }
-
-        if (hasScanned[4])
-        {
             AnglerFish.color = new Color32(255, 255, 255, 255);
-            anglerfish.text = "Anglerfish";
-        }
-        else
-        {
-            AnglerFish.color = new Color32(0, 0, 0, 100);
-        }
-
-        if (hasScanned[5])
-        {
-            SwordFish_NotScanned.sprite = Swordfish_Scanned;
-            swordfish.text = "Swordfish";
-        }
-        else
-        {
-            SwordFish.color = new Color32(0, 0, 0, 100);
-        }
-    }
-    public void BackToFishDex()
-    {
-        SeaTurtleInfo.SetActive(false);
-        NautilusInfo.SetActive(false);
-        Fishdex.SetActive(true);
-
-        //sets all to false
-        for (int i = 0; i < opened.Length; i++) { opened[i] = false; }
-    }
-
-    public void NextOrPrevious()
-    {
-        if(opened[0])
-        {
-            DrawingNautilus.SetActive(!DrawingNautilus.activeSelf);
-            PictureNautilus.SetActive(!PictureNautilus.activeSelf);
-        }
-        if(opened[1])
-        {
-            DrawingTurtle.SetActive(!DrawingTurtle.activeSelf);
-            PictureTurtle.SetActive(!PictureTurtle.activeSelf);
+            headings[12].GetComponent<Text>().text = "AnglerFish";
         }
     }
 
-
-    public void OpenNautilus()
-    {
-        if (hasScanned[0])
-        {
-            opened[0] = true;
-            NautilusInfo.SetActive(true);
-            Fishdex.SetActive(false);
-        }
-    }
-    public void OpenSeaTurtle()
-    {
-        if (hasScanned[1])
-        {
-            opened[1] = true;
-            SeaTurtleInfo.SetActive(true);
-            Fishdex.SetActive(false);
-        }
-    }
 
 }
