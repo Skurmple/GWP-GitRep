ff62d084fbc8e8163f080c1f81ea057c6fdc05fe
Swueld <vojtech.kuba.cz@gmail.com>
2022-04-15 13:51:22 +0100

    Fish+trash animation wip

--- Ocean Cleanup Net Prototype/Assets/C#/CrustTrashSpawner.cs
+++ Ocean Cleanup Net Prototype/Assets/C#/CrustTrashSpawner.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class CrustTrashSpawner : MonoBehaviour
 {
 
-    public Rigidbody2D plasticBottle, plasticBag;
-    public Rigidbody2D metalScrap, metalCan;
-    public Rigidbody2D glassBottle;
     Vector3 spawningPosition;
     Vector3 droppedPosition;
+
+    public Rigidbody2D[] trashList;
 
     void Start()
     {
@@ -36,46 +35,10 @@ public class CrustTrashSpawner : MonoBehaviour
                 yield return new WaitForSeconds(Random.Range(0.2f, 0.5f));
 
                 spawningPosition = new Vector3(transform.position.x + Random.Range(-17, 17), transform.position.y - 2, 0);
+                int trashChoice = Random.Range(1, trashList.Length);
 
-                //Generate a random number between 0 and 100
-                int trashChoice = Random.Range(0, 101);
-
-                //If random number is less than 50, clone plastic trash, this means plastic trash is the most likely to appear (50%), then metal (35%) and then glass (15%)
-                if (trashChoice <= 50)
-                {
-                    int plasticChoice = Random.Range(0, 100);
-
-                    if (plasticChoice < 50)
-                    {
-                        Rigidbody2D clone;
-                        clone = Instantiate(plasticBottle, spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25)))); 
-                    }
-                    else
-                    {
-                        Rigidbody2D clone;
-                        clone = Instantiate(plasticBag, spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25))));
-                    }
-                }
-                else if (trashChoice > 50 && trashChoice <= 85)
-                {
-                    int metalChoice = Random.Range(0, 100);
-
-                    if (metalChoice < 50)
-                    {
-                        Rigidbody2D clone;
-                        clone = Instantiate(metalScrap, spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25))));
-                    }
-                    else
-                    {
-                        Rigidbody2D clone;
-                        clone = Instantiate(metalCan, spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25))));
-                    }
-                }
-                else if (trashChoice > 85)
-                {
-                    Rigidbody2D clone;
-                    clone = Instantiate(glassBottle, spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25))));
-                }
+                Rigidbody2D clone;
+                clone = Instantiate(trashList[trashChoice - 1], spawningPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(-25, 25))));
             }
         }
         Destroy(gameObject);
